9b91717770b3ec00d87c9509e696dc2d1feb94ac
Amy <amy.greene029@gmail.com>
2021-10-08 11:19:47 +0100

    sort drop down working

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AppointmentInfo from './components/AppointmentInfo';
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
 
   const filteredAppointment = appointmentList.filter(
     item =>{
@@ -17,7 +19,14 @@ function App() {
         item.aptNotes.toLowerCase().includes(query.toLowerCase()) 
       )
     }
-  )
+  ).sort((a,b) => {
+    let orders = (orderBy=== 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() <
+      b[sortBy].toLowerCase() ?
+      -1 * orders: 1 * orders
+    )
+  })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -36,7 +45,12 @@ function App() {
       <h1 className="text-5xl mb-3"> <BiCalendar className="inline-block text-red-400"/>Your Appointments</h1>
       <AddAppointment />
       <Search query={query}
-        onQueryChange={myQuery => setQuery(myQuery)}/>
+        onQueryChange={myQuery => setQuery(myQuery)}
+        orderBy={orderBy}
+        onOrderByChange = {mySort => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange = {mySort => setSortBy(mySort)}
+        />
       
       <ul className="divide-y divide-grey-200">
         {filteredAppointment.map(appointment => (
