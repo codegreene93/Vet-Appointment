49f388e9d03b561a0084bd244f0bbbbe3ce23565
Amy <amy.greene029@gmail.com>
2021-10-08 10:25:37 +0100

    search bar working

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointment = appointmentList.filter(
+    item =>{
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase()) 
+      )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -24,13 +35,14 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3"> <BiCalendar className="inline-block text-red-400"/>Your Appointments</h1>
       <AddAppointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)}/>
       
       <ul className="divide-y divide-grey-200">
-        {appointmentList.map(appointment => (
+        {filteredAppointment.map(appointment => (
            <AppointmentInfo key={appointment.id} appointment={appointment} onDeleteAppointment={
              appointmentId => 
-             setAppointmentList(appointmentList.filter(appointment => appointment.id != appointmentId))
+             setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId))
            }/>
         ))
         }
